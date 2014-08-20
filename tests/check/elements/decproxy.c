2173496f7135337557d9308d99503dd66469fc92
hoonhee.lee <hoonhee.lee@lge.com>
2014-08-20 19:50:28 +0900

    tests: decproxy: Add test case for active event
    decproxy will deploy a real decoder after receiving "active" event.
    
    Change-Id: I7d88f1a8e2fd5ae3019b2dcb93ff0b886530234c
    Reviewed-on: https://gpro.palm.com/60681
    Reviewed-by: Jeongseok Kim <jeongseok.kim@lge.com>
    Tested-by: Jeongseok Kim <jeongseok.kim@lge.com>

--- tests/check/elements/decproxy.c
+++ tests/check/elements/decproxy.c
@@ -111,12 +111,151 @@ GST_START_TEST (test_media_info)
 
 GST_END_TEST;
 
+/* Fake decoder */
+static GType gst_fake_h264_decoder_get_type (void);
+
+#undef parent_class
+#define parent_class fake_h264_decoder_parent_class
+typedef struct _GstFakeH264Decoder GstFakeH264Decoder;
+typedef GstElementClass GstFakeH264DecoderClass;
+
+struct _GstFakeH264Decoder
+{
+  GstElement parent;
+};
+
+G_DEFINE_TYPE (GstFakeH264Decoder, gst_fake_h264_decoder, GST_TYPE_ELEMENT);
+
+static void
+gst_fake_h264_decoder_class_init (GstFakeH264DecoderClass * klass)
+{
+  static GstStaticPadTemplate sink_templ = GST_STATIC_PAD_TEMPLATE ("sink",
+      GST_PAD_SINK, GST_PAD_ALWAYS,
+      GST_STATIC_CAPS ("video/x-h264, " "stream-format=(string) byte-stream"));
+  static GstStaticPadTemplate src_templ = GST_STATIC_PAD_TEMPLATE ("src",
+      GST_PAD_SRC, GST_PAD_ALWAYS,
+      GST_STATIC_CAPS ("video/x-raw"));
+  GstElementClass *element_class = GST_ELEMENT_CLASS (klass);
+
+  gst_element_class_add_pad_template (element_class,
+      gst_static_pad_template_get (&sink_templ));
+  gst_element_class_add_pad_template (element_class,
+      gst_static_pad_template_get (&src_templ));
+  gst_element_class_set_metadata (element_class,
+      "FakeH264Decoder", "Codec/Decoder/Video", "yep", "me");
+}
+
+static GstFlowReturn
+gst_fake_h264_decoder_sink_chain (GstPad * pad, GstObject * parent,
+    GstBuffer * buf)
+{
+  GstElement *self = GST_ELEMENT (parent);
+  GstPad *otherpad = gst_element_get_static_pad (self, "src");
+  GstFlowReturn ret = GST_FLOW_OK;
+
+  buf = gst_buffer_make_writable (buf);
+
+  ret = gst_pad_push (otherpad, buf);
+
+  gst_object_unref (otherpad);
+
+  return ret;
+}
+
+static void
+gst_fake_h264_decoder_init (GstFakeH264Decoder * self)
+{
+  GstPad *pad;
+
+  pad =
+      gst_pad_new_from_template (gst_element_class_get_pad_template
+      (GST_ELEMENT_GET_CLASS (self), "sink"), "sink");
+  gst_pad_set_chain_function (pad, gst_fake_h264_decoder_sink_chain);
+  gst_element_add_pad (GST_ELEMENT (self), pad);
+
+  pad =
+      gst_pad_new_from_template (gst_element_class_get_pad_template
+      (GST_ELEMENT_GET_CLASS (self), "src"), "src");
+  gst_element_add_pad (GST_ELEMENT (self), pad);
+}
+
+static GstStaticPadTemplate src_pad_template =
+    GST_STATIC_PAD_TEMPLATE ("src", GST_PAD_SRC,
+    GST_PAD_ALWAYS,
+    GST_STATIC_CAPS ("video/x-h264;"));
+
 /*
  * When active event is coming, decproxy should deploy a decode element
  * and connect.
  */
 GST_START_TEST (test_active_decodable_element)
 {
+  GstPluginFeature *feature;
+  GstElement *decproxy;
+  GstElement *dec_elem;
+  GstCaps *caps, *caps_sinkpad, *caps_srcpad;
+  GstEvent *event;
+  GstStructure *s;
+  gchar *name;
+  GstPad *mysrcpad, *sinkpad;
+  GstStateChangeReturn state_ret;
+
+  gst_element_register (NULL, "fakeh264dec", GST_RANK_PRIMARY + 99,
+      gst_fake_h264_decoder_get_type ());
+
+  feature =
+      gst_registry_find_feature (gst_registry_get (), "vdecproxy",
+      GST_TYPE_ELEMENT_FACTORY);
+  fail_unless (feature != NULL, "Failed to get feature of vdecproxy");
+  gst_plugin_feature_set_rank (feature, GST_RANK_PRIMARY + 100);
+
+  decproxy = gst_element_factory_make ("vdecproxy", "vdecproxy");
+  fail_unless (decproxy != NULL, "Failed to create decproxy element");
+
+  /* link srcpad and decproxy */
+  mysrcpad = gst_check_setup_src_pad (decproxy, &src_pad_template);
+  gst_pad_set_active (mysrcpad, TRUE);
+
+  /* sinkpad of decproxy has a finxed-caps through the padtemplate of srcpad */
+  sinkpad = gst_element_get_static_pad (decproxy, "sink");
+  caps_sinkpad = gst_pad_get_allowed_caps (sinkpad);
+  caps_srcpad = gst_pad_get_pad_template_caps (mysrcpad);
+  fail_unless (gst_caps_is_equal (caps_sinkpad, caps_srcpad),
+      "Failed to set fixed-caps of sinkpad in decproxy");
+  gst_caps_unref (caps_srcpad);
+  gst_caps_unref (caps_sinkpad);
+  gst_object_unref (sinkpad);
+
+  /* push caps */
+  caps = gst_caps_new_empty_simple ("video/x-h264");
+  gst_pad_push_event (mysrcpad, gst_event_new_caps (caps));
+
+  state_ret = gst_element_set_state (decproxy, GST_STATE_PAUSED);
+  fail_unless (state_ret != GST_STATE_CHANGE_FAILURE);
+
+  /* puch active event to generate actual decoder element */
+  s = gst_structure_new ("acquired-resource",
+      "active", G_TYPE_BOOLEAN, TRUE, NULL);
+  fail_if (s == NULL);
+  event = gst_event_new_custom (GST_EVENT_CUSTOM_UPSTREAM, s);
+  fail_unless (gst_element_send_event (decproxy, event));
+
+  g_object_get (decproxy, "actual-decoder", &dec_elem, NULL);
+  fail_unless (dec_elem != NULL, "Failed to get decoder element");
+  name = gst_element_get_name (dec_elem);
+
+  fail_unless (g_str_has_prefix (name, "fakeh264dec"),
+      "Unexpected decoder element has deployed");
+
+  fail_unless_equals_int (gst_element_set_state (decproxy, GST_STATE_NULL),
+      GST_STATE_CHANGE_SUCCESS);
+
+  /* clean up */
+  gst_caps_unref (caps);
+  g_free (name);
+  gst_check_teardown_src_pad (decproxy);
+  gst_object_unref (dec_elem);
+  gst_object_unref (decproxy);
 }
 
 GST_END_TEST;
