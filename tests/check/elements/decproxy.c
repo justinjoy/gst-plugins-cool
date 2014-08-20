bab9f797e3b6e361cdf5ed69c9d80e780b70713b
hoonhee.lee <hoonhee.lee@lge.com>
2014-08-20 19:25:16 +0900

    tests: decproxy: Add test case for media info
    :Detailed Notes:
    When caps event is coming, decproxy should trans caps's information to
    media-info application message and post. Then it should be confirmed
    by bus.
    
    Change-Id: Id00e8b69a226617366a39d51e7016603b8800034
    Reviewed-on: https://gpro.palm.com/60476
    Reviewed-by: Jeongseok Kim <jeongseok.kim@lge.com>
    Tested-by: Jeongseok Kim <jeongseok.kim@lge.com>

--- tests/check/elements/decproxy.c
+++ tests/check/elements/decproxy.c
@@ -25,6 +25,30 @@
 #include <gst/gst.h>
 #include <gst/check/gstcheck.h>
 
+static gboolean
+compare_media_field (GQuark field_id, const GValue * value, gpointer user_data)
+{
+  const GstStructure *s2;
+  gchar *value_str1, *value_str2;
+
+  /* No need to compare stream-id and type */
+  if ((g_strcmp0 (g_quark_to_string (field_id), "stream-id") == 0)
+      || (g_strcmp0 (g_quark_to_string (field_id), "type") == 0))
+    return TRUE;
+
+  s2 = user_data;
+  value_str1 = gst_value_serialize (value);
+  value_str2 = gst_value_serialize (gst_structure_id_get_value (s2, field_id));
+
+  if (g_strcmp0 (value_str1, value_str2) != 0)
+    return FALSE;
+
+  g_free (value_str1);
+  g_free (value_str2);
+
+  return TRUE;
+}
+
 /*
  * When caps event is coming, decproxy should trans caps's information to
  * media-info application message and post.
@@ -32,6 +56,57 @@
  */
 GST_START_TEST (test_media_info)
 {
+  GstElement *decproxy;
+  GstCaps *caps_list[3];
+  GstBus *bus;
+  GstMessage *message;
+  gint i;
+
+  decproxy = gst_element_factory_make ("vdecproxy", "vdecproxy");
+  fail_unless (decproxy != NULL, "Failed to create decproxy element");
+
+  bus = gst_bus_new ();
+  gst_element_set_bus (decproxy, bus);
+
+  /* setup caps */
+  caps_list[0] =
+      gst_caps_new_simple ("video/x-h264", "stream-format", G_TYPE_STRING,
+      "byte-stream", NULL);
+  caps_list[1] =
+      gst_caps_new_simple ("video/x-h264", "stream-format", G_TYPE_STRING,
+      "avc", NULL);
+  caps_list[2] =
+      gst_caps_new_simple ("video/x-h264", "stream-format", G_TYPE_STRING,
+      "byte-stream", "alignment", G_TYPE_STRING, "nal", NULL);
+
+  /* 
+   * pushs caps event 3 times.
+   * Then, passed media-info from caps should be confirmed by bus message.
+   */
+  for (i = 0; i < 3; i++) {
+    fail_unless (gst_element_send_event (decproxy,
+            gst_event_new_caps (caps_list[i])));
+
+    message = gst_bus_poll (bus, GST_MESSAGE_APPLICATION, 2 * GST_SECOND);
+    fail_unless (message != NULL, "Failed to receive media-info msg");
+    if (gst_message_has_name (message, "media-info")) {
+      const GstStructure *s1;
+      GstStructure *s2;
+
+      s1 = gst_message_get_structure (message);
+      s2 = gst_caps_get_structure (caps_list[i], 0);
+      fail_unless (gst_structure_foreach (s1, compare_media_field, s2),
+          "Found not matched value");
+    }
+    gst_message_unref (message);
+  }
+
+  /* cleanup */
+  gst_element_set_bus (decproxy, NULL);
+  gst_object_unref (bus);
+  for (i = 0; i < 3; i++)
+    gst_caps_unref (caps_list[i]);
+  gst_object_unref (decproxy);
 }
 
 GST_END_TEST;
